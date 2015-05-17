ff53cffc5cef6d4bb6ca6beacda362b000e42b79
Emin Tham <emintham@gmail.com>
2015-05-17 15:59:41 -0400

    Adds processes; process test fails

--- concurrent.py
+++ concurrent.py
@@ -4,28 +4,34 @@ Adapted from:
 https://www.caktusgroup.com/blog/2009/05/26/testing-django-views-for-concurrency-issues/
 """
 import threading
+import multiprocessing
 
 
-def concurrently(num_threads, times_each):
+def concurrently(num_thingies, times_each, model):
     def test_concurrently_decorator(test_func):
         def wrapper(*args, **kwargs):
             exceptions = []
             def call_test_func(t_num):
                 try:
                     for _ in range(times_each):
-                        print 'Thread {}: {}'.format(t_num,
-                                                     test_func(*args, **kwargs))
+                        print '{} {}: {}'.format(model,
+                                                 t_num,
+                                                 test_func(*args, **kwargs))
                 except Exception as e:
                     exceptions.append(e)
                     raise
 
-            threads = [threading.Thread(target=call_test_func, args=(i, ))
-                       for i in range(num_threads)]
+            if model == 'thread':
+                thingies = [threading.Thread(target=call_test_func, args=(i, ))
+                           for i in range(num_thingies)]
+            else:
+                thingies = [multiprocessing.Process(target=call_test_func, args=(i, ))
+                            for i in range(num_thingies)]
 
-            for t in threads:
+            for t in thingies:
                 t.start()
 
-            for t in threads:
+            for t in thingies:
                 t.join()
 
             if exceptions:
--- tests.py
+++ tests.py
@@ -1,30 +1,52 @@
+from threading import Lock
 from unittest import TestCase
+import multiprocessing
 import time
-import random
 
 from concurrent import concurrently
 
 
 class Foo(object):
     foo = 0
-    def inc(self):
-        s = self.foo + 1
-        time.sleep(random.random())
-        self.foo = s
+
+    def __init__(self, model='thread'):
+        self.model = model
+
+    def inc(self, lock):
+        with lock:
+            s = self.foo + 1
+            time.sleep(0.01)
+            self.foo = s
+
+        if self.model == 'process':
+            print 'process'
         return s
 
 
 class ConcurrencyTests(TestCase):
-    def test_concurrency(self):
-        num_threads = 10
-        times_each = 10
-        expected = num_threads * times_each
+    def setUp(self):
+        self.num_thingies = 5
+        self.num_times_each = 5
+        self.expected = self.num_thingies * self.num_times_each
 
+    def test_concurrency_with_threads(self):
         foo = Foo()
+        lock = Lock()
 
-        @concurrently(num_threads, times_each)
+        @concurrently(self.num_thingies, self.num_times_each, 'thread')
         def do_stuff(f):
-            return f.inc()
+            return f.inc(lock)
 
         do_stuff(foo)
-        self.assertEqual(foo.foo, expected)
+        self.assertEqual(foo.foo, self.expected)
+
+    def test_concurrency_with_processes(self):
+        foo = Foo('process')
+        lock = multiprocessing.Lock()
+
+        @concurrently(self.num_thingies, self.num_times_each, 'process')
+        def do_stuff(f):
+            return f.inc(lock)
+
+        do_stuff(foo)
+        self.assertEqual(foo.foo, self.expected)
